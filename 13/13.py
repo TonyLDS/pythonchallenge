4f216eec4e829e81b96f1262617952ed50689583
Tony <lzq920733541@126.com>
2016-12-27 11:57:29 +0800

    13

---
+++ 13/13.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 27 11:56:06 2016
+
+@author: luzhangqin
+"""
+
+#http://www.pythonchallenge.com/pc/return/evil.htmi
+"""
+evil = open("evil2.gfx","rb").read()
+
+open("evil_first.jpg","wb").write(evil[::5])
+open("evil_secode.png","wb").write(evil[1::5])
+open("evil_third.gif","wb").write(evil[2::5])
+open("evil_fouth.png","wb").write(evil[3::5])
+open("evil_fifth.jpg","wb").write(evil[4::5])
+"""
