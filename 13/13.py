613fdfaeeb972c486385234f817dcff6efa84d65
Tony <lzq920733541@126.com>
2016-12-27 11:58:48 +0800

    13.py

--- 13/13.py
+++ 13/13.py
@@ -6,7 +6,7 @@ Created on Tue Dec 27 11:56:06 2016
 """
 
 #http://www.pythonchallenge.com/pc/return/evil.htmi
-"""
+
 evil = open("evil2.gfx","rb").read()
 
 open("evil_first.jpg","wb").write(evil[::5])
@@ -14,4 +14,3 @@ open("evil_secode.png","wb").write(evil[1::5])
 open("evil_third.gif","wb").write(evil[2::5])
 open("evil_fouth.png","wb").write(evil[3::5])
 open("evil_fifth.jpg","wb").write(evil[4::5])
-"""
